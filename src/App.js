a77846e29d2e5b054ddc09a3c18bb2464ba28b90
temmy2003 <mukhtartemmy@gmail.com>
2024-08-06 13:56:32 +0100

    attendance

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LoginPage from './pages/auth/login_page';
+
+
+// ----------------------------------------------------------------
+// Setup routes for the application pages
+function App() {
+  return (
+    <div className="App">
+<LoginPage/>
+    </div>
+  );
+}
+
+export default App;
